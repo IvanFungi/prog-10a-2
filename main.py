4f5392517d1a5a4af8f8be2e1b7c2bcf21e21984
AeSiL <114918736+IvanFungi@users.noreply.github.com>
2022-10-10 12:05:58 +0000

    Uzdevums 2 :>

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-MIK=int(input("Knife in the back, or the number in my chat: "))
-if MIK>40:
-  print("beeg")
+from math import *
+a=int(input("Unga number: "))
+b=int(input("Bubga numeral: "))
+c=int(input("Dunga counting symbol: "))
+d=b**2-4*a*c
+if d<0:
+ print("Rootless")
+elif d==0:
+  x=-b/2*a
+  print("one root =",x)
 else:
-  print("smol")
+ x1=(-b+sqrt(d))/2*a
+ x2=(-b-sqrt(d))/2*a
+ print("Two roots 1x =",x1,"2x =",x2)
