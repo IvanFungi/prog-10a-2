75c1ebb506b7d3309bb8fecee680a6f157c37477
AeSiL <114918736+IvanFungi@users.noreply.github.com>
2022-10-17 11:53:34 +0000

    Otrs uzdevums!

--- main.py
+++ main.py
@@ -15,19 +15,51 @@
 
 
 
-A=int(input("First edge's degree: "))
-B=int(input("Second edge's degree: "))
-C=int(input("Third edge's degree: "))
-T=A+B+C
-if T>180:
-  print("Incorrect degree ammount")
-elif T<180:
-  print("Incorrect degre ammount")
-elif A==B and B==C:
-  print("Equal triangle")
-elif A>90 or B>90 or C>90:
-  print("Obtuse triangle")
-elif A==90 or B==90 or C==90:
-  print("Right triangle")
-else:
-  print("Just kinda a triangle")
+# A=int(input("First edge's degree: "))
+# B=int(input("Second edge's degree: "))
+# C=int(input("Third edge's degree: "))
+# T=A+B+C
+# if T>180:
+#   print("Incorrect degree ammount")
+# elif T<180:
+#   print("Incorrect degre ammount")
+# elif A==B and B==C:
+#   print("Equal triangle")
+# elif A>90 or B>90 or C>90:
+#   print("Obtuse triangle")
+# elif A==90 or B==90 or C==90:
+#   print("Right triangle")
+# else:
+#   print("Just kinda a triangle")
+
+
+
+D=int(input("Give me number a you please: "))
+if D==1:
+ print("January")
+elif D==2:
+  print("February")
+elif D==3:
+  print("March")
+elif D==4:
+  print("April")
+elif D==5:
+  print("May")
+elif D==6:
+  print("June")
+elif D==7:
+  print("July")
+elif D==8:
+  print("August")
+elif D==9:
+  print("September")
+elif D==10:
+  print("October")
+elif D==11:
+  print("November")
+elif D==12:
+  print("December")
+elif D<1:
+ print("Not a month")
+elif D>12:
+  print("Not a month")
