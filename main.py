65bf4b96628e33bd0c450a9e6c0c152e90eea20e
AeSiL <114918736+IvanFungi@users.noreply.github.com>
2022-11-14 13:22:31 +0000

    everything :0

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# from math import *
+from math import *
 # a=int(input("Unga number: "))
 # b=int(input("Bubga numeral: "))
 # c=int(input("Dunga counting symbol: "))
@@ -85,27 +85,181 @@
 
 
 
-E=int(input("your numer: "))
-if E<0:
-  print("It's a negative number.")
-  if E<0 and E>-10:
-    print("It's single digit.")
-  elif E<-9 and E>-100:
-    print("It's double digit.")
-  elif E<-99 and E>-1000:
-    print("It's triple digit.")
-  elif E<-999:
-    print("Quadruple and more digit numbers")
+# E=int(input("your numer: "))
+# if E<0:
+#   print("It's a negative number.")
+#   if E<0 and E>-10:
+#     print("It's single digit.")
+#   elif E<-9 and E>-100:
+#     print("It's double digit.")
+#   elif E<-99 and E>-1000:
+#     print("It's triple digit.")
+#   elif E<-999:
+#     print("Quadruple and more digit numbers")
 
-elif E>0:
-  print("It's a pozitive number.")
-  if E>0 and E<10:
-    print("It's single digit.")
-  elif E>9 and E<100:
-    print("It's double digit.")
-  elif E>99 and E<1000:
-    print("It's triple digit.")
-  elif E>999:
-    print("Quadruple and more digit numbers")
-else:
-  print("Null")
+# elif E>0:
+#   print("It's a pozitive number.")
+#   if E>0 and E<10:
+#     print("It's single digit.")
+#   elif E>9 and E<100:
+#     print("It's double digit.")
+#   elif E>99 and E<1000:
+#     print("It's triple digit.")
+#   elif E>999:
+#     print("Quadruple and more digit numbers")
+# else:
+#   print("Null")
+
+"""
+T=5
+T=complex(T)
+print(T)
+"""
+
+#E="Jojo"
+#print(len(E))
+
+# txt="        Klon non out    "
+# Q=txt.strip()
+# print(Q)
+
+# year=1234
+# txt="Hi, I'm from the year {}"
+# print(txt.format(year))
+
+#print(bool("abc"))
+##True
+
+#if a==b: print("kooki")
+
+#print("yo") if 2==2 else print("yont")
+
+# i = 1
+# while i < 6:
+#   if i == 3:
+#     break
+# i += 1
+##break stops
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   if x == "banana":
+#     continue
+#   print(x)
+##continue skips banana
+
+# for x in range(1009):
+#   print(x)
+
+# def my_function():
+#   print("Hello from a function")
+# my_function()
+
+# def my_function(x):
+#   return x+5
+##???
+
+# def my_function(*kids):
+#   print("The youngest child is " + kids[2])
+##If you do not know the number of arguments that will be passed into your function, use prefix * in the function definition
+
+# def my_function(**kid):
+#   print("His last name is " + kid["lname"])
+##If you do not know the number of keyword arguments that will be passed into your function, use prefix ** in the function definition
+
+
+
+# text=input("Insert rteatasdhoauytrftgyhujkl;wasdfzghj: ")
+# thing=0
+# for letter in text:
+#   if letter=="a":
+#     thing+=1
+# print("letter a number=", thing)
+
+
+
+# Hren=[1,2,-67,3425,2,4,5,55,5,5,5,-5]
+
+# Length=len(Hren)
+# Maximum=max(Hren)
+# Minimum=min(Hren)
+# ken=Hren.count(5)
+
+# print("Five's (for fun)= ",ken)
+# print("Length= ",Length)
+# print("Maximum= ",Maximum)
+# print("Minimum= ",Minimum)
+
+# sum=0
+# for ken in Hren:
+#   sum+=ken
+# print("Summa= ", sum)
+
+# Multiplication=1
+# for ken in Hren:
+#   print("Current Element= ", ken, " Current Multiplication= ", Multiplication)
+#   Multiplication=Multiplication*ken
+# print("Multiplication result= ", Multiplication)
+
+
+
+# spende=int(input("Give a random nubmer'ino: "))
+# thling=0
+# for ki in range(0,spende):
+#   if ki%2!==0:
+#     thling+=ki
+# print(thling)
+# for ki in range(0,spende):
+#   if ki%2==0:
+#   thling+1=ki
+#   print(thling)
+
+
+
+# gin=[2,34,1,5,2]
+# Slugmo=0
+# THING=1
+# for leme in gin:
+#   print(leme)
+# for leme in gin:
+#   if leme%2!=0:
+#     Slugmo+=leme
+# print("Sungo= ",Slugmo)
+# for leme in gin:
+#   print(gin[len(gin)-THING])
+#   THING+=1
+
+
+
+# sklin=0
+# while sklin<=100:
+#   # if sklin%2==0:
+#   if sklin%2!=0:
+#     print(sklin)
+#   sklin+=1
+
+
+
+# skrinkle=-5
+# while skrinkle<=25:
+#   print(cos(skrinkle)**2)
+#   skrinkle+=2
+
+
+
+xo=int(input("Gimme numbyr: "))
+ching2_0 = []
+ching = []
+chink = 0
+while chink<=xo:
+  chink+=1
+  if chink%2!=0:
+    ching.append(chink)
+  else:
+    ching2_0.append(chink)
+print("Here's all your odd numbers", ching)
+print("Here's all your even numbers", ching2_0)
